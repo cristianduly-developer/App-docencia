c85ba50ec4b3c1407d50bbfd497f709e4394e024
Cristian <cristianduly@gmail.com>
2026-06-10 15:51:09 -0300

    build: bundle actualizado con turnos/vicedirector

--- dist/sw.js
+++ dist/sw.js
@@ -1,7 +1,7 @@
 // Service Worker básico para PWA
 // Permite instalar la app en el celular como app nativa
 
-const CACHE_NAME = 'aye-v1781115227280';
+const CACHE_NAME = 'aye-v1781117458918';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
